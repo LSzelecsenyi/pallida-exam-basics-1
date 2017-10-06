6826ce3082697f008761ad5f01b45a4addadf3d7
LSzelecsenyi <szelelaci@gmail.com>
2017-10-06 13:15:51 +0200

    candyshop started

---
+++ src/CandyShop.java
@@ -0,0 +1,16 @@
+public class CandyShop {
+
+    int sugar;
+    int money;
+
+    public CandyShop(int sugar, int money) {
+        this.money = money;
+        this.sugar = sugar;
+    }
+
+    public void createCandie() {
+
+    }
+
+
+}
